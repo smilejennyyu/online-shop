2eb590b97dc9bef3bf64358c7b6c7f545933eb63
smilejennyyu <jennyyu163@yahoo.ca>
2018-08-04 19:01:30 -0400

    finish login page

--- project.py
+++ project.py
@@ -170,9 +170,8 @@ def gdisconnect():
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        flash("Logged out successfully.")
+        return redirect(url_for('showStores'))
     else:
         response = make_response(json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
